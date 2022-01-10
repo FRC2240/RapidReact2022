ef40fe03f4db8c6fd863f79f28a2d294f4d9bd70
Jeff Bezos Stonerbot <cynosure@disroot.org>
2022-01-09 17:05:24 -0700

    Declared Drivebase
    Added:
    includes for Robot.h
    Talon Motors (drivebase)
    Differntial drive
    Added motor groups
    
    -- Commit made by Jeff Bezos Stonerbot
    (AKA cynosure or Cynosure-NUL) <cynosure@disroot.org>
    My PGP key can be found at https://cynosure.neocities.org/about
    Fly safe
    
     On branch speedy-boi
     Changes to be committed:
    	modified:   src/main/cpp/Robot.cpp
    	modified:   src/main/include/Robot.h
    	new file:   vendordeps/Phoenix.json
    	new file:   vendordeps/REVLib.json

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -6,6 +6,32 @@
 
 #include <string>
 
+#include "rev/CANSparkMax.h"
+#include "ctre/Phoenix.h"
+
+#include <frc/DriverStation.h>
+#include <frc/Timer.h>
+
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+
+#include "frc/smartdashboard/SmartDashboard.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+
+#include <frc/GenericHID.h>
+#include <frc/drive/RobotDriveBase.h>
+#include <frc/RobotController.h>
+#include <frc/motorcontrol/MotorController.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/motorcontrol/MotorControllerGroup.h>
+
+#include <wpi/sendable/Sendable.h>
+#include <wpi/sendable/SendableHelper.h>
+#include "frc/drive/RobotDriveBase.h"
+#include "frc/drive/DifferentialDrive.h"
+#include <frc/XboxController.h>
+
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
 
@@ -21,10 +47,33 @@ class Robot : public frc::TimedRobot {
   void DisabledPeriodic() override;
   void TestInit() override;
   void TestPeriodic() override;
+// void InitializeDashboard();
+// void InitializePIDControllers();
+// void ReadDashboard();
 
  private:
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Default";
   const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
+
+//So long, Joystick.h!
+  frc::XboxController m_stick{0};
+// A second controler 
+  frc::XboxController m_stick_climb{1};
+
+  WPI_TalonFX m_frontRightMotor = {1};
+  WPI_TalonFX m_backRightMotor = {2};
+  WPI_TalonFX m_frontLeftMotor = {3}; 
+  WPI_TalonFX m_backLeftMotor = {4}; 
+
+// Tonk drive
+  frc::MotorControllerGroup m_leftDrive{m_frontLeftMotor, m_backLeftMotor};
+  frc::MotorControllerGroup m_rightDrive{m_frontRightMotor, m_backRightMotor};
+
+  frc::DifferentialDrive m_drive{m_leftDrive, m_rightDrive};
+
+
+// A nice arrangment of motors
+
 };
